ec4335b633f00d1acea796816ae9d7bf15f8c361
Prashant Rajoriya <prashant.rajoriya21@gmail.com>
2018-10-07 13:18:08 +0530

    Updates

---
+++ precache-manifest.c2c7959c5ebaa8a6d4f075121704e062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67122e2cdde974b7bf57",
+    "url": "/Random-Quote-Machine/static/js/runtime~main.67122e2c.js"
+  },
+  {
+    "revision": "9f4c425ea67193216f88",
+    "url": "/Random-Quote-Machine/static/js/main.9f4c425e.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/Random-Quote-Machine/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "9f4c425ea67193216f88",
+    "url": "/Random-Quote-Machine/static/css/main.08924bb4.chunk.css"
+  },
+  {
+    "revision": "f7b489b2f665c52a9b9e3b32fcf292ca",
+    "url": "/Random-Quote-Machine/index.html"
+  }
+];
